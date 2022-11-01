c959eda831b047e804d788323b6405b4bd83cd46
preferkim <shineecard5@gmail.com>
2022-11-02 01:37:19 +0900

    feat: Order Entity 가 FixedAmountVoucher 에 의존

--- src/main/java/org/prgms/kdt/Order.java
+++ src/main/java/org/prgms/kdt/Order.java
@@ -1,5 +1,6 @@
 package org.prgms.kdt;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -7,25 +8,21 @@ public class Order {
     private final UUID orderId;
     private final UUID customerId;
     private final List<OrderItem> orderItems;
-    private long discountAmount;
+    private FixedAmountVoucher fixedAmountVoucher;
     private OrderStatus orderStatus = OrderStatus.ACCEPTED;
 
-    public Order(UUID orderId, UUID customerId, List<OrderItem> orderItems, long discountAmount) {
+    public Order(UUID orderId, UUID customerId, ArrayList<OrderItem> orderItems, long discountAmount) {
         this.orderId = orderId;
         this.customerId = customerId;
         this.orderItems = orderItems;
-        this.discountAmount = discountAmount;
+        this.fixedAmountVoucher = new FixedAmountVoucher(discountAmount);
     }
 
     public long totalAmount() {
         var beforeDiscount = orderItems.stream()
                 .map(v -> v.getProductPrice() * v.getQuantity())
                 .reduce(0L, Long::sum);
-        return beforeDiscount - discountAmount;
-    }
-
-    public void setDiscountAmount(long discountAmount) {
-        this.discountAmount = discountAmount;
+        return fixedAmountVoucher.discount(beforeDiscount);
     }
 
     public void setOrderStatus(OrderStatus orderStatus) {
