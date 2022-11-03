7bacc6299170e30243717c7a67194b50e0b85c51
preferkim <shineecard5@gmail.com>
2022-11-04 00:18:03 +0900

    test: Circular Dependency 예제

---
+++ src/main/java/org/prgms/kdt/CircularDepTester.java
@@ -0,0 +1,41 @@
+package org.prgms.kdt;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+class A {
+    private final B b;
+
+    A(B b) {
+        this.b = b;
+    }
+}
+
+class B {
+    private final A a;
+
+    B(A a) {
+        this.a = a;
+    }
+}
+
+class CircularConfig {
+
+    @Bean
+    public A a(B b) {
+        return new A(b);
+    }
+
+    @Bean
+    public B b(A a) {
+        return new B(a);
+    }
+}
+
+public class CircularDepTester {
+
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext annotationConfigApplicationContext
+                = new AnnotationConfigApplicationContext(CircularConfig.class);
+    }
+}
