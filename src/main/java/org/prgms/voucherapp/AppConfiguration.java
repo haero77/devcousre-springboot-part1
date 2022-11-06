ebf5d1aa761225a81dd1299fff3ded2430b0fa69
preferkim <shineecard5@gmail.com>
2022-11-06 23:22:43 +0900

    Refactor: basePackages를 사용할 때, 패키지를 문자열로 설정함으로써 생기는 문제를 basePackagesClasses를 사용하여 해결

--- src/main/java/org/prgms/voucherapp/AppConfiguration.java
+++ src/main/java/org/prgms/voucherapp/AppConfiguration.java
@@ -1,10 +1,13 @@
 package org.prgms.voucherapp;
 
+import org.prgms.voucherapp.order.Order;
+import org.prgms.voucherapp.voucher.Voucher;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages = {"org.prgms.voucherapp.order", "org.prgms.voucherapp.voucher"})
+@ComponentScan(basePackageClasses = {Order.class, Voucher.class})
+// basePackageClasses: 해당 클래스가 속해있는 패키지 기준으로 Scan
 public class AppConfiguration { // 최상위 폴더에 있을때 ComponentScan과 관련하여 오류가 안 난다.
 
 }
