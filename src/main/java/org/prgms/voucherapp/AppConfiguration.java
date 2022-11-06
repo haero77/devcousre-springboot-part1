b54c6fae7252ae5ad8fc4cbd97a0e30172d76791
preferkim <shineecard5@gmail.com>
2022-11-06 23:20:16 +0900

    Refactor: basePackages 를 사용하여 스캔 범위 지정

--- src/main/java/org/prgms/voucherapp/AppConfiguration.java
+++ src/main/java/org/prgms/voucherapp/AppConfiguration.java
@@ -1,8 +1,10 @@
 package org.prgms.voucherapp;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan(basePackages = {"org.prgms.voucherapp.order", "org.prgms.voucherapp.voucher"})
 public class AppConfiguration { // 최상위 폴더에 있을때 ComponentScan과 관련하여 오류가 안 난다.
 
 }
